94a6c926470f1dc4ac6e7dd073413e3c7d2749c5
Filipe Bezerra <filipebzerra@gmail.com>
2016-02-12 13:34:53 -0200

    Novos atributos da entidade Etapa.

--- app/src/main/java/br/com/libertsolutions/crs/app/step/WorkStep.java
+++ app/src/main/java/br/com/libertsolutions/crs/app/step/WorkStep.java
@@ -1,5 +1,7 @@
 package br.com.libertsolutions.crs.app.step;
 
+import android.support.annotation.NonNull;
+
 /**
  * .
  *
@@ -7,15 +9,57 @@ package br.com.libertsolutions.crs.app.step;
  * @version 0.1.0, 10/02/2016
  * @since 0.1.0
  */
-public class WorkStep {
-    String name;
+public class WorkStep implements Comparable<WorkStep> {
+    int mOrder;
+
+    String mName;
+
+    int mKind;
+
+    int mGoForward;
+
+    public int getOrder() {
+        return mOrder;
+    }
+
+    public WorkStep setOrder(int order) {
+        mOrder = order;
+        return this;
+    }
 
     public String getName() {
-        return name;
+        return mName;
     }
 
     public WorkStep setName(String name) {
-        this.name = name;
+        mName = name;
         return this;
+    }
+
+    public int getKind() {
+        return mKind;
+    }
+
+    public WorkStep setKind(int kind) {
+        mKind = kind;
+        return this;
+    }
+
+    public int getGoForward() {
+        return mGoForward;
+    }
+
+    public WorkStep setGoForward(int goForward) {
+        mGoForward = goForward;
+        return this;
+    }
+
+    @Override
+    public int compareTo(@NonNull  WorkStep another) {
+        if (getOrder() == another.getOrder()) {
+            return 0;
+        }
+
+        return getOrder() < another.getOrder() ? -1 : 1;
     }
 }
