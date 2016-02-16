5e5962586d3607aeffc75edb25a34bcab3737a82
Filipe Bezerra <filipebzerra@gmail.com>
2016-02-16 11:51:08 -0200

    Remoção do Spinner na tela de Check-ins.

--- app/src/main/java/br/com/libertsolutions/crs/app/checkin/CheckinActivity.java
+++ app/src/main/java/br/com/libertsolutions/crs/app/checkin/CheckinActivity.java
@@ -7,17 +7,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Spinner;
 import br.com.libertsolutions.crs.app.R;
-import br.com.libertsolutions.crs.app.android.spinner.SpinnerAdapter;
 import br.com.libertsolutions.crs.app.base.BaseActivity;
 import br.com.libertsolutions.crs.app.recyclerview.DividerDecoration;
 import br.com.libertsolutions.crs.app.recyclerview.OnClickListener;
 import br.com.libertsolutions.crs.app.recyclerview.OnTouchListener;
 import br.com.libertsolutions.crs.app.step.WorkStep;
-import br.com.libertsolutions.crs.app.step.WorkSteps;
 import butterknife.ButterKnife;
 import java.util.List;
 
@@ -54,11 +49,8 @@ public class CheckinActivity extends BaseActivity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mWorkStep = getIntent().getParcelableExtra(EXTRA_DATA);
-
-        setupSpinner();
 
         final List<Checkin> checkins = Checkins.getDataSet(mWorkStep.getType());
 
@@ -71,42 +63,6 @@ public class CheckinActivity extends BaseActivity implements OnClickListener {
                 new DividerDecoration(this));
         checkingsView.addOnItemTouchListener(
                 new OnTouchListener(this, checkingsView, this));
-    }
-
-    private void setupSpinner() {
-        final List<WorkStep> steps = WorkSteps.getDataSet();
-        final String actualStepName = mWorkStep.getName();
-
-        String[] stepList = new String[steps.size()];
-        int i = 0;
-        int actualStepPosition = 0;
-        for(WorkStep step : steps) {
-            stepList[i] = step.getName();
-
-            if (step.getName().equals(actualStepName)) {
-                actualStepPosition = i;
-            }
-
-            i++;
-        }
-
-        // Setup spinner
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(new SpinnerAdapter(
-                mToolbarAsActionBar.getContext(), stepList));
-
-        spinner.setSelection(actualStepPosition, true);
-
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
     }
 
     @Override
