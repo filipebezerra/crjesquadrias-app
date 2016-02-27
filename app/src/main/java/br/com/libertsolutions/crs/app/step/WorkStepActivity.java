5afa60ab053f164e80c110398b3fb6764d2761ce
Filipe Bezerra <filipebzerra@gmail.com>
2016-02-27 13:31:10 -0300

    Tela de Obras em visualização modo Grid na orientação Paisagem.

--- app/src/main/java/br/com/libertsolutions/crs/app/step/WorkStepActivity.java
+++ app/src/main/java/br/com/libertsolutions/crs/app/step/WorkStepActivity.java
@@ -2,18 +2,20 @@ package br.com.libertsolutions.crs.app.step;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import br.com.libertsolutions.crs.app.R;
 import br.com.libertsolutions.crs.app.android.activity.BaseActivity;
-import br.com.libertsolutions.crs.app.android.recyclerview.DividerDecoration;
+import br.com.libertsolutions.crs.app.android.recyclerview.GridDividerDecoration;
 import br.com.libertsolutions.crs.app.android.recyclerview.OnClickListener;
 import br.com.libertsolutions.crs.app.android.recyclerview.OnTouchListener;
 import br.com.libertsolutions.crs.app.checkin.CheckinActivity;
-import butterknife.ButterKnife;
+import butterknife.Bind;
 
 /**
  * .
@@ -25,6 +27,8 @@ import butterknife.ButterKnife;
 public class WorkStepActivity extends BaseActivity implements OnClickListener {
 
     private WorkStepAdapter mWorkStepAdapter;
+    
+    @Bind(android.R.id.list) RecyclerView mWorkStepsView;
 
     @Override
     protected int provideLayoutResource() {
@@ -44,15 +48,12 @@ public class WorkStepActivity extends BaseActivity implements OnClickListener {
     protected void onCreate(Bundle inState) {
         super.onCreate(inState);
 
-        RecyclerView stagesView = ButterKnife.findById(this, android.R.id.list);
-        stagesView.setLayoutManager(new LinearLayoutManager(this));
-        stagesView.setHasFixedSize(true);
-        stagesView.setAdapter(
-                mWorkStepAdapter = new WorkStepAdapter(this));
-        stagesView.addItemDecoration(
-                new DividerDecoration(this));
-        stagesView.addOnItemTouchListener(
-                new OnTouchListener(this, stagesView, this));
+        changeListLayout(getResources().getConfiguration());
+        mWorkStepsView.setHasFixedSize(true);
+        mWorkStepsView.setAdapter(mWorkStepAdapter = new WorkStepAdapter(this));
+        mWorkStepsView.addItemDecoration(new GridDividerDecoration(this));
+        mWorkStepsView.addOnItemTouchListener(
+                new OnTouchListener(this, mWorkStepsView, this));
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setSubtitle(getString(R.string.steps_in_running,
@@ -72,5 +73,19 @@ public class WorkStepActivity extends BaseActivity implements OnClickListener {
     @Override
     public void onLongPress(View view, int position) {
 
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        changeListLayout(newConfig);
+    }
+
+    private void changeListLayout(Configuration configuration) {
+        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mWorkStepsView.setLayoutManager(new GridLayoutManager(this, 3));
+        } else {
+            mWorkStepsView.setLayoutManager(new LinearLayoutManager(this));
+        }
     }
 }
