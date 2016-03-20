73a1d9d292a4ae402ae2a03a7376a6cbfeea487b
Filipe Bezerra <filipebzerra@gmail.com>
2016-03-20 16:35:19 -0300

    Issue #21 - Integrando com Realm e RxJava/RxAndroid.

---
+++ app/src/main/java/br/com/libertsolutions/crs/app/application/App.java
@@ -0,0 +1,25 @@
+package br.com.libertsolutions.crs.app.application;
+
+import android.app.Application;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * .
+ *
+ * @author Filipe Bezerra
+ * @version 0.1.0, 18/03/2016
+ * @since 0.1.0
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        RealmConfiguration configuration = new RealmConfiguration.Builder(this)
+                .name("crs.realm")
+                .build();
+        Realm.deleteRealm(configuration);
+        Realm.setDefaultConfiguration(configuration);
+    }
+}
--- app/src/main/java/br/com/libertsolutions/crs/app/retrofit/RetrofitHelper.java
+++ app/src/main/java/br/com/libertsolutions/crs/app/retrofit/RetrofitHelper.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import br.com.libertsolutions.crs.app.settings.SettingsHelper;
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.realm.RealmObject;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -55,9 +60,23 @@ public class RetrofitHelper {
                     .addInterceptor(createInterceptorWithAuthKey(authKey))
                     .build();
 
+            final Gson gson = new GsonBuilder()
+                    .setExclusionStrategies(new ExclusionStrategy() {
+                        @Override
+                        public boolean shouldSkipField(FieldAttributes f) {
+                            return f.getDeclaringClass().equals(RealmObject.class);
+                        }
+
+                        @Override
+                        public boolean shouldSkipClass(Class<?> clazz) {
+                            return false;
+                        }
+                    })
+                    .create();
+
             sRetrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .client(httpClient)
                     .build();
