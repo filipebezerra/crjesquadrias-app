b893a2fd9888806b5fde374802667258a5431b00
Filipe Bezerra <filipebzerra@gmail.com>
2016-06-02 21:44:57 -0300

    Inicialização estruturada das bibliotecas em uso.

--- app/src/main/java/br/com/libertsolutions/crs/app/application/App.java
+++ app/src/main/java/br/com/libertsolutions/crs/app/application/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import br.com.libertsolutions.crs.app.BuildConfig;
 import br.com.libertsolutions.crs.app.utils.logging.CrashReportingTree;
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -13,22 +14,16 @@ import timber.log.Timber;
  * .
  *
  * @author Filipe Bezerra
- * @version 0.1.0, 18/03/2016
+ * @version 0.1.0, 02/06/2016
  * @since 0.1.0
  */
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         initLoggingWithTimber();
-
-        Fabric.with(this, new Crashlytics());
-
-        RealmConfiguration configuration = new RealmConfiguration.Builder(this)
-                .name("crs.realm")
-                .build();
-        Realm.setDefaultConfiguration(configuration);
+        initCrashReportingWithFabric();
+        initDataStorageWithRealm();
     }
 
     private void initLoggingWithTimber() {
@@ -37,5 +32,19 @@ public class App extends Application {
         } else {
             Timber.plant(new CrashReportingTree());
         }
+    }
+
+    private void initCrashReportingWithFabric() {
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+        Fabric.with(this, crashlyticsKit);
+    }
+
+    private void initDataStorageWithRealm() {
+        RealmConfiguration configuration = new RealmConfiguration.Builder(this)
+                .name("crs.realm")
+                .build();
+        Realm.setDefaultConfiguration(configuration);
     }
 }
