c3b2ceaf4b27c719730a7d22376e01b047a83a5a
Filipe Bezerra <filipebzerra@gmail.com>
2016-07-03 02:22:02 -0300

    #38 - Correção da exceção java.lang.OutOfMemoryError.

--- app/src/main/java/br/com/libertsolutions/crs/app/launchscreen/LaunchScreenActivity.java
+++ app/src/main/java/br/com/libertsolutions/crs/app/launchscreen/LaunchScreenActivity.java
@@ -7,11 +7,9 @@ import br.com.libertsolutions.crs.app.android.activity.BaseActivity;
 import br.com.libertsolutions.crs.app.utils.navigation.NavigationHelper;
 
 /**
- * .
- *
  * @author Filipe Bezerra
- * @version 0.1.0, 20/03/2016
- * @since 0.1.0, 22/01/2016
+ * @version 0.2.0
+ * @since 0.1.0
  */
 public class LaunchScreenActivity extends BaseActivity {
     private static final int SPLASH_TIME = 3000;
@@ -24,16 +22,8 @@ public class LaunchScreenActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle inState) {
         super.onCreate(inState);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                NavigationHelper.navigateToLoginScreen(LaunchScreenActivity.this);
-            }
-        }, SPLASH_TIME);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+        new Handler().postDelayed(
+                () -> NavigationHelper.navigateToLoginScreen(LaunchScreenActivity.this),
+                SPLASH_TIME);
     }
 }
